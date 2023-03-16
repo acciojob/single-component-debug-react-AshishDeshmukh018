db7d9a1b6c34c46adc06b47480b5ead2382eb009
AshishDeshmukh018 <126492153+AshishDeshmukh018@users.noreply.github.com>
2023-03-16 17:52:07 +0530

    second commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,2 @@
-import React from 'react';
-
-function App() {
-  return (
-    <div>
-        <p>Now I can render any React component on any DOM node I want using ReactDOM.render</p>
-    </div>
-  );
-}
-
-export default App;
-
-
-
+<p>Now I can render any React component on any DOM node I want using ReactDOM.render</p>
+ 
