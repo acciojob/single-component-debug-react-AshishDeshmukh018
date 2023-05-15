c5cf1eefbf4dea06d8ea80ded8d0c03732f5b5ce
Ashish Deshmukh <ashishdeshmukh658@gmail.com>
2023-05-15 14:26:48 +0530

    submitting code-Mon May 15 2023 14:26:47 GMT+0530 (India Standard Time)

--- src/components/App.js
+++ src/components/App.js
@@ -3,7 +3,7 @@ import React from 'react';
 const App = () => {
   return (
     <div>
-      
+      <p>I can now create full fleged React components and render it on the DOM node</p>
     </div>
   );
 };
